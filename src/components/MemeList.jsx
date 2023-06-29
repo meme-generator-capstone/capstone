9f98d6b4fce6978ff43a4a23bc0c4981c6440de7
Alainna Amato <ampalmato@gmail.com>
2023-06-29 09:51:27 -0700

    working memelist component added

--- src/components/MemeList.jsx
+++ src/components/MemeList.jsx
@@ -1,3 +1,33 @@
 import React from "react"
 
 // props are the memelist state
+
+export default function MemeList(props) {
+    function handleClick() {
+        const meme = props.meme
+        props.setMemeList(prevMemes => {
+            return ([...prevMemes, meme])
+        })
+    }
+
+    let memes = props.memeList.map(meme => {
+       return (
+        <li className="meme">
+            <img src={meme.randomImage} className="meme--image" />
+            <h2 className="meme--text top">{meme.topText}</h2>
+            <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </li>
+       )
+    })
+    return (
+        <div className="memelist">
+            <button className="add-meme"
+                    onClick={handleClick}>
+                    Add meme to list
+            </button>
+            <ul>
+                {memes}
+            </ul>
+        </div>
+    )
+}
